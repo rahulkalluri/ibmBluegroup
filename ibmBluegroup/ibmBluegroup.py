0fb841e32d1d15ec4df0a02b803818e95c037eab
Rahul Kalluri <rahul.kalluri@ibm.com>
2020-03-18 12:32:56 -0500

    Formatting to PEP8, update version number

--- ibmBluegroup/ibmBluegroup.py
+++ ibmBluegroup/ibmBluegroup.py
@@ -1,51 +1,54 @@
- #!/usr/bin/env python3
- # -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import httplib2
 from xml.dom.minidom import parse
 import xml.dom.minidom
 
-#author: guojial@cn.ibm.com
-#version: v1.05
-#Date:   2017-10-27
-#Last Modified by: rahul.kalluri@ibm.com
-#Last Modified time: 2020-03-18
+
+# author: guojial@cn.ibm.com
+# version: v1.06
+# Date:   2017-10-27
+# Last Modified by: rahul.kalluri@ibm.com
+# Last Modified time: 2020-03-18
 
 class ibmBluegroup:
 
-	#check the user exsit in group
-	def userInGroup(self, intranetID, groupname):
+    # check the user exist in group
+    def userInGroup(self, intranetID, groupname):
 
-		http = httplib2.Http()
-		content = http.request("https://bluepages.ibm.com/tools/groups/groupsxml.wss?task=listMembers&group="+groupname, "GET")
-		#print(content)
+        http = httplib2.Http()
+        content = http.request(
+            "https://bluepages.ibm.com/tools/groups/groupsxml.wss?task=listMembers&group=" + groupname, "GET")
+        # print(content)
 
-		DOMTree = xml.dom.minidom.parseString(content[1])
-		collection = DOMTree.documentElement
+        DOMTree = xml.dom.minidom.parseString(content[1])
+        collection = DOMTree.documentElement
 
-		#print(collection.getElementsByTagName("member")[0].childNodes[0].data)
-		flag = False
-		for member in collection.getElementsByTagName("member"):
-			if (intranetID.lower() == member.childNodes[0].data.lower()):
-				flag = True
-				break
-			else:
-				flag = False
-		#print(flag)
-		return flag
+        # print(collection.getElementsByTagName("member")[0].childNodes[0].data)
+        flag = False
+        for member in collection.getElementsByTagName("member"):
+            if intranetID.lower() == member.childNodes[0].data.lower():
+                flag = True
+                break
+            else:
+                flag = False
+        # print(flag)
+        return flag
 
-	def listMembers(self, groupname):
+    def listMembers(self, groupname):
 
-		http = httplib2.Http()
-		content = http.request("https://bluepages.ibm.com/tools/groups/groupsxml.wss?task=listMembers&group="+groupname, "GET")
-		#print(content)
+        http = httplib2.Http()
+        content = http.request(
+            "https://bluepages.ibm.com/tools/groups/groupsxml.wss?task=listMembers&group=" + groupname, "GET")
+        # print(content)
 
-		DOMTree = xml.dom.minidom.parseString(content[1])
-		collection = DOMTree.documentElement
+        DOMTree = xml.dom.minidom.parseString(content[1])
+        collection = DOMTree.documentElement
 
-		#print(collection.getElementsByTagName("member")[0].childNodes[0].data)
-		members = [];
-		for member in collection.getElementsByTagName("member"):
-			members.append(member.childNodes[0].data)
-		#print(members)
-		return members
+        # print(collection.getElementsByTagName("member")[0].childNodes[0].data)
+        members = [];
+        for member in collection.getElementsByTagName("member"):
+            members.append(member.childNodes[0].data)
+        # print(members)
+        return members
